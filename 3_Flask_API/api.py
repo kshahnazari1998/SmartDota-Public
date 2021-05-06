69d647a1ac6fb418659192308bbe3803d661a31f
kshahnazari1998 <kshahnazari@gmail.com>
2021-05-05 22:57:15 -0700

    updated flask model to an ensamble model

--- 3_Flask_API/api.py
+++ 3_Flask_API/api.py
@@ -7,12 +7,20 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 import operator
+import numpy as np
+
+import pickle
+
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import PolynomialFeatures
 
 app = flask.Flask(__name__)
-#app.config["DEBUG"] = True
+
 CORS(app)
 # only trigger SSLify if the app is running on Heroku
 Talisman(app)
+
+
 class predict_game:
     def __init__(self, radiant_picks, dire_picks, side, role):
         self.radiant_picks = radiant_picks
@@ -24,6 +32,7 @@ class predict_game:
         # return self.side
         radiant, dire = self.trim_data(self.radiant_picks, self.dire_picks)
         picks = self.transform_data(radiant, dire)
+        picks_linear = self.transform_data_linear(radiant, dire)
         # prob = self.model_predict(picks)
         pickfile = open("picklist/" + self.role + ".txt", "r")
         ids_str = pickfile.readlines()
@@ -35,10 +44,18 @@ class predict_game:
             if picks[hero] == 0:
                 if self.side == "radiant":
                     picks[hero] = -1
-                    prob = self.model_predict(picks)
+                    picks_linear[hero] = 1
+                    prob1 = self.model_predict(picks)
+                    prob2 = self.model_predict_linear(picks_linear)
+                    prob = (prob1 + prob2) / 2
+                    picks_linear[hero] = 0
                 else:
                     picks[hero] = 1
-                    prob = 1 - self.model_predict(picks)
+                    picks_linear[hero + 150] = 1
+                    prob1 = 1 - self.model_predict(picks)
+                    prob2 = 1 - self.model_predict_linear(picks_linear)
+                    prob = (prob1 + prob2) / 2
+                    picks_linear[hero + 150] = 0
                 picks[hero] = 0
                 win_prob.append((hero, prob))
 
@@ -74,6 +91,14 @@ class predict_game:
             picks[dir] = 1
         return picks
 
+    def transform_data_linear(self, radiant_picks, dire_picks):
+        picks = np.zeros(300)
+        for rad in radiant_picks:
+            picks[rad] = 1
+        for dir in dire_picks:
+            picks[dir + 150] = 1
+        return picks
+
     def model_predict(self, picks):
         predictor = GamePredictor_final()
         predictor.load_state_dict(torch.load("model.pth"))
@@ -82,69 +107,77 @@ class predict_game:
         output = predictor(pick).item()
         return output
 
+    def model_predict_linear(self, picks):
+        predictor = pickle.load(open("linear_model", "rb"))
+        poly = PolynomialFeatures(degree=2)
+        x = poly.fit_transform(np.array([picks]))
+        output = predictor.predict_proba(x)
+        return output[0][0]
+
 
 class GamePredictor_final(nn.Module):
     def __init__(self):
         super().__init__()
-        self.l1 = nn.Linear(150, 100)
-        self.l3 = nn.Linear(100, 1)
+        self.l1 = nn.Linear(150, 50)
+        self.l2 = nn.Linear(50, 50)
+        self.l3 = nn.Linear(50, 1)
 
     def forward(self, x):
         # Pass the input tensor through each of our operations
         x = F.relu(self.l1(x))
-        # x = F.relu(self.l2(x))
+        x = F.relu(self.l2(x))
         x = self.l3(x)
         return torch.sigmoid(x)
 
 
-
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "Just a simple API"
 
-@app.route('/api/v1/predictor',methods=['GET'])
+
+@app.route("/api/v1/predictor", methods=["GET"])
 def api():
     try:
-        side = str(request.args['side'])
-        role = str(request.args['role'])
-        
-        r1 = int(request.args['r1'])
-        r2 = int(request.args['r2'])
-        r3 = int(request.args['r3'])
-        r4 = int(request.args['r4'])
-        r5 = int(request.args['r5'])
+        side = str(request.args["side"])
+        role = str(request.args["role"])
 
-        d1 = int(request.args['r1'])
-        d2 = int(request.args['r2'])
-        d3 = int(request.args['r3'])
-        d4 = int(request.args['r4'])
-        d5 = int(request.args['r5'])
+        r1 = int(request.args["r1"])
+        r2 = int(request.args["r2"])
+        r3 = int(request.args["r3"])
+        r4 = int(request.args["r4"])
+        r5 = int(request.args["r5"])
 
-        radiant_picks = [r1,r2,r3,r4,r5]
-        dire_picks = [d1,d2,d3,d4,d5]
+        d1 = int(request.args["r1"])
+        d2 = int(request.args["r2"])
+        d3 = int(request.args["r3"])
+        d4 = int(request.args["r4"])
+        d5 = int(request.args["r5"])
+
+        radiant_picks = [r1, r2, r3, r4, r5]
+        dire_picks = [d1, d2, d3, d4, d5]
 
         res = predict_game(radiant_picks, dire_picks, side, role).predict()
-    except:
+    except Exception as e:
+        print(e)
         res = "There was an error in Api Call"
-        
+
     return jsonify(res)
 
-app.config['UPLOAD_FOLDER'] = "."
 
-@app.route("/file", methods=['POST','PUT'])
+app.config["UPLOAD_FOLDER"] = "."
+
+
+@app.route("/file", methods=["POST", "PUT"])
 def upload():
-    if request.method == 'POST':
-        password = request.form['pass']
+    if request.method == "POST":
+        password = request.form["pass"]
         if str(password) == "xxx":
-            f = request.files['app']
+            f = request.files["app"]
             f.save((f.filename))
             return "Success"
         else:
             return "Wrong Pass"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
-
-
-
-
